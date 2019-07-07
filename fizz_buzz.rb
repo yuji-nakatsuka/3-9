ffc17b6ce93b7f4cef22b5b854ff4c86e7a88194
中塚優治 <U-Z@nakatsukayuuchi-no-MacBook-Air-2.local>
2019-07-07 19:26:57 +0900

    [Finish]3-9

---
+++ fizz_buzz.rb
@@ -0,0 +1,19 @@
+def fizz_buzz (number)
+	if number%3==0 && number%5==0
+		return "FizzBuzz"
+	elsif number%5==0
+		return "Buzz"
+	elsif number%3==0
+		return "Fizz"
+	else
+		return number.to_s
+end
+end
+
+puts "1 以上の数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は、、、"
+
+puts fizz_buzz(input)
